e6180aeda02dc0bb82d10355ac95abf00552ee48
rohitbindal <rbindal1233@gmail.com>
2018-11-01 19:22:07 +0530

    Inlcudes Singly, Singly-Circular, Doubly and Doubly-Circular Linked Lists.

---
+++ Linked_List/cmake-build-debug/CMakeFiles/extra.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/extra.dir/LinkedList.c.obj"
+  "extra.pdb"
+  "extra.exe"
+  "extra.exe.manifest"
+  "libextra.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/extra.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
