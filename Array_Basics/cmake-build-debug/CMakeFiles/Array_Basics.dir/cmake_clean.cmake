25090a889061c17046006633d9d2a42309ebd88d
rohitbindal <33802874+rohitbindal@users.noreply.github.com>
2018-11-02 13:46:40 +0530

    Add files via upload

---
+++ Array_Basics/cmake-build-debug/CMakeFiles/Array_Basics.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Array_Basics.dir/main.c.obj"
+  "Array_Basics.pdb"
+  "Array_Basics.exe"
+  "Array_Basics.exe.manifest"
+  "libArray_Basics.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Array_Basics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Array_Basics/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void number(int a[], int size){
+    int i,j,temp;
+    for(i=0;i<size;i++){
+        temp = 0;
+        for(j=0;j<size;j++){
+            if(a[i]<a[j]){
+                temp=a[j];
+                a[j]=a[i];
+                a[i]=temp;
+            }
+        }
+    }
+    printf("\nThe Largest Number is: %d:\nThe Second Largest Number is: %d",a[size-1],a[size-2]);
+}
+
+int main(){
+    printf("Enter the size of the array: ");
+    int s;
+    scanf("%d",&s);
+    printf("\nEnter it's elements.");
+    int a[s];
+    for(int i = 0;i<s;i++){
+        scanf("%d",&a[i]);
+    }
+    number(a,s);
+    return 0;
+}
